7aee4b3bb5bda1fe70f693dd5d7683936af2e02e
Mahdi silawi <Mahdisilawi@gmail.com>
2023-05-27 16:48:58 +0330

    upload mycontact win form proj to github

---
+++ Services/ContactsRipository.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace MyContacts
+{
+    internal class ContactsRipository : IContactsReopsitory
+    {
+
+        private string connectionString = "Data Source=.;Initial Catalog=Contact_DB;Integrated Security=true;";
+        public bool Delete(int contactId)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                string query = "Delete From MyContacts where ContactId=@ID";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@ID",contactId);
+                connection.Open();
+                command.ExecuteNonQuery();
+                return true;
+            }
+            catch 
+            {
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public bool Insert(string name, string family, string mobile, string email, int age, string address)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                
+                string query = "Insert Into MyContacts (Name,Family,Email,Age,Mobile,Address) values(@Name,@Family,@Email,@Age,@Mobile,@Address) ";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Name", name);
+                command.Parameters.AddWithValue("@Family", family);
+                command.Parameters.AddWithValue("@Email", email);
+                command.Parameters.AddWithValue("@Age", age);
+                command.Parameters.AddWithValue("@Mobile", mobile);
+                command.Parameters.AddWithValue("@Address", address);
+                connection.Open();
+                command.ExecuteNonQuery();
+               
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public DataTable Search(string parameter)
+        {
+            string query = "select * from MyContacts where Name like @parameter or Family like @parameter ";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+            adapter.SelectCommand.Parameters.AddWithValue("@parameter", "%" + parameter +"%");
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+            return data;
+        }
+
+        public DataTable SelectAll()
+        {
+            string query = "select * from MyContacts";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+            return data;
+        }
+
+        public DataTable SelectRow(int contactId)
+        {
+            string query = "select * from MyContacts where ContactId=" + contactId;
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+            return data;
+        }
+
+        public bool Update(int contactId, string name, string family, string mobile, string email, int age, string address)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                string query = "Update MyContacts Set Name=@Name,Family=@Family,Mobile=@mobile,Email=@Email,Age=@Age,Address=@Address where ContactId=@ID ";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@ID", contactId);
+                command.Parameters.AddWithValue("@Name", name);
+                command.Parameters.AddWithValue("@Family", family);
+                command.Parameters.AddWithValue("@Email", email);
+                command.Parameters.AddWithValue("@Age", age);
+                command.Parameters.AddWithValue("@Mobile", mobile);
+                command.Parameters.AddWithValue("@Address", address);
+                connection.Open();
+                command.ExecuteNonQuery();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ frmAddOrEdit.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyContacts
+{
+    public partial class frmAddOrEdit : Form
+    {
+        IContactsReopsitory ripository;
+
+        public int contactId = 0;
+        public frmAddOrEdit()
+        {
+            InitializeComponent();
+            ripository = new ContactsRipository();
+        }
+
+        private void frmAddOrEdit_Load(object sender, EventArgs e)
+        {
+            if (contactId==0)
+            {
+                this.Text = "افزودن شخص جدید";
+            }
+            else
+            {
+                this.Text = "ویرایش شخص ";
+                DataTable dt = ripository.SelectRow(contactId);
+                txtName.Text = dt.Rows[0][1].ToString();
+                txtFamily.Text = dt.Rows[0][2].ToString();
+                txtMobile.Text = dt.Rows[0][3].ToString();
+                txtEmail.Text = dt.Rows[0][4].ToString();
+                txtAge.Text = dt.Rows[0][5].ToString();
+                txtAddress.Text = dt.Rows[0][6].ToString();
+                btnSubmit.Text = "ویرایش";
+            }
+        }
+
+        bool ValidateInputs()
+        {
+
+
+            if (txtName.Text == "")
+            {
+                MessageBox.Show("لطفا نام را وارد کنید ", "هشدار", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            if (txtFamily.Text == "")
+            {
+                MessageBox.Show("لطفا نام  خانوادگی را وارد کنید ", "هشدار", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            if (txtMobile.Text == "")
+            {
+                MessageBox.Show("لطفا موبایل را وارد کنید ", "هشدار", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            if (txtAge.Value == 0)
+            {
+                MessageBox.Show("لطفا سن را وارد کنید ", "هشدار", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            if (txtEmail.Text == "")
+            {
+                MessageBox.Show("لطفا ایمیل را وارد کنید ", "هشدار", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            
+            if (txtAddress.Text == "")
+            {
+                MessageBox.Show("لطفا آدرس را وارد کنید ", "هشدار", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+           
+            return true;
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (ValidateInputs())
+            {
+                bool isSuccess;
+                if (contactId==0)
+                {
+                    isSuccess = ripository.Insert(txtName.Text, txtFamily.Text, txtMobile.Text, txtEmail.Text, (int)txtAge.Value, txtAddress.Text);
+                }
+                else
+                {
+                    isSuccess = ripository.Update(contactId, txtName.Text, txtFamily.Text, txtMobile.Text, txtEmail.Text, (int)txtAge.Value, txtAddress.Text);
+                }
+                if (isSuccess)
+                {
+                    MessageBox.Show("عملیات با موفقیت انجام شد","موفقیت",MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    DialogResult = DialogResult.OK;
+                }
+                else
+                {
+                    MessageBox.Show("عملیات با شکست مواجه شد", "خطا", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
